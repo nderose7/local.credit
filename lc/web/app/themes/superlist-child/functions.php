035aa440bb9b330e5d9d34b8d70a054ec487c9b3
Nick <nderose7@gmail.com>
2017-01-16 22:08:24 -0700

    Add superlist theme

---
+++ lc/web/app/themes/superlist-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Superlist child functions and definitions
+ *
+ * @package Superlist Child
+ * @since Superlist Child 1.0.0
+ */
---
+++ lc/web/app/themes/superlist/templates/banner-types/featured-image.php
@@ -0,0 +1,3 @@
+<div class="detail-banner" data-background-image="<?php echo esc_attr( wp_get_attachment_url( get_post_thumbnail_id() ) ); ?>">
+    <?php get_template_part( 'templates/content-listing-banner-info' ); ?>
+</div><!-- /.detail-banner -->
